da9931698e9cf7c0466560ef4b9ff8db83f8f3f7
MacStudent <macstudent@20x-imacxx-m-509.local>
2018-08-16 14:37:18 -0400

    design

---
+++ EmployeePayroll/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  EmployeePayroll
+//
+//  Created by MacStudent on 2018-08-16.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
--- EmployeePayroll/LoginViewController.swift
+++ EmployeePayroll/LoginViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.performSegue(withIdentifier: "LoginViewController", sender: self)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
